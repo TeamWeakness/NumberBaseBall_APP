d23c707c9b67f8b36d374a18b6f064274c8aa224
Huni <zxca1520@gmail.com>
2020-11-13 21:37:28 +0900

    2020-11-13 commit!

--- app/src/main/cpp/playball.cpp
+++ app/src/main/cpp/playball.cpp
@@ -5,11 +5,6 @@
 #include "myHeader.h"
 typedef struct numberBB_info
 {
-    //int viewNumAry[9][10][10]; // 언젠가 필요할까?
-    int viewNum[999];
-    /*int fisrtNum[9];
-    int secondNum[10];
-    int thirdNum[10];*/
     int randNum[3];
     int inputNum[3];
     int strike;
@@ -17,18 +12,74 @@ typedef struct numberBB_info
 
 }NumberBB;
 
-int dataInit(NumberBB* nbb);
-void initDataPrint(NumberBB nbb);
+int playBall(NumberBB* nbb);
+int nRound(NumberBB* nbb);
+
+static NumberBB nbb = { 0, };
 
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_weakness_numberbaseball_PlayballActivity_main(JNIEnv* env,jobject /* this */)
+JNIEXPORT jint JNICALL // 이건 메소드마다 추가 해줘야 하는건가?? 왜 메소드별로 없으면 오류가 날까..?
+Java_com_weakness_numberbaseball_PlayballActivity_main(JNIEnv* env,jobject /* this */, jint inputNum ,jint firstNum, jint secondNum, jint thirdNum)
 {
-    return env->NewStringUTF("이렇게 하는건가??");
+    int res[2] = {0, };
+
+    nbb.strike;
+    nRound(&nbb);
+    res[0] = nbb.strike;
+    res[1] = nbb.ball;
+
+    return (jint)res;
 }
 
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_weakness_numberbaseball_PlayballActivity_dataInit(JNIEnv* env,jclass )
+{
+    // 랜덤 값 초기화
+    int row = 1;
+    srand((unsigned int)time(NULL));
+    nbb.randNum[0] = rand() % 9 + 1;
+    while(1)
+    {
+        nbb.randNum[row] = rand() % 9;
+        if (nbb.randNum[row - 1] == nbb.randNum[row])
+        {
+            continue;
+        }
+        else
+        {
+            if (row == 2)
+            {
+                break;
+            }
+            row++;
+        }
+    }
+}
 
-extern "C" JNIEXPORT jstring JNICALL // 이건 메소드마다 추가 해줘야 하는건가?? 왜 메소드별로 없으면 오류가 날까..?
-Java_com_weakness_numberbaseball_PlayballActivity_test(JNIEnv *env, jobject thiz) {
-// TODO: implement test()
+int nRound(NumberBB* nbb)
+{
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            if (row == col && nbb->randNum[row] == nbb->inputNum[col])
+            {
+                nbb->strike++;
+            }
+            else if (row != col && nbb->randNum[row] == nbb->inputNum[col])
+            {
+                nbb->ball++;
+            }
+
+        }
+    }
+    if (nbb->strike == 3)
+    {
+        return 3;
+    }
+    else
+    {
+        return 0;
+    }
 }
--- app/src/main/java/com/weakness/numberbaseball/PlayballActivity.java
+++ app/src/main/java/com/weakness/numberbaseball/PlayballActivity.java
@@ -4,30 +4,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PlayballActivity extends AppCompatActivity {
     Intent playball_resultIntent;
     Button playball_backBtn;
-    Button playball_checkBtn;
+    Button playball_submitBtn;
     EditText playball_inputNum;
-    EditText playball_inputBall;
-    EditText playball_inputStrike;
+    TextView playball_resStrike;
+    TextView playball_resBall;
+    public int inputNum;
+    public int inputStrike;
+    public int inputBall;
+    public int[] res;
+    public int firstNum;
+    public int secondNum;
+    public int thirdNum;
+    public int callCnt;
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("playball");
+        System.loadLibrary("numberBaseball-lib");
     }
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native int main(int inputNum,int firstNum, int secondNum, int thirdNum);
+    public native static void dataInit();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playball);
 
+        res = new int[2];
+
+        playball_inputNum = findViewById(R.id.playball_et_inputNum);
+
+        playball_submitBtn = findViewById(R.id.playball_btn_submitBtn);
+        playball_submitBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                inputNum = Integer.parseInt(playball_inputNum.getText().toString());
+                firstNum = inputNum / 100;
+                secondNum = inputNum % 100 / 10;
+                thirdNum = inputNum % 100 % 10;
+                if(firstNum == secondNum || secondNum == thirdNum || firstNum == thirdNum)
+                {
+                    Toast.makeText(PlayballActivity.this,"중복된 숫자는 입력할 수 없습니다.",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    res = main(inputNum,firstNum,secondNum,thirdNum);
+                    //Log.i("확인용","이거 데이터 오니? : " + res);
+
+                }
+            }
+        });
+
         playball_backBtn = findViewById(R.id.playball_btn_backBtn);
         playball_backBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
 
@@ -39,13 +85,6 @@ public class PlayballActivity extends AppCompatActivity {
             }
         });
     }
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-
-    public native String main();
-    public native String test();
 
 }
 // 새 C/C++ 파일을 프로젝트에 추가한 후에 CMake를 구성하여 네이티브 라이브러리에 포함해야 합니다. 이거 중요하다!!
