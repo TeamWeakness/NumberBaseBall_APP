7c019fc18058c13f8d27e544fa73047b66abc0a7
Huni <zxca1520@gmail.com>
2020-12-02 13:27:27 +0900

    2020-12-02 add reSet function !

--- app/src/main/java/com/weakness/numberbaseball/PlayballActivity.java
+++ app/src/main/java/com/weakness/numberbaseball/PlayballActivity.java
@@ -25,6 +25,7 @@ public class PlayballActivity extends AppCompatActivity implements TextView.OnEd
     Button playball_backBtn;
     Button playball_submitBtn;
     Button playball_hintBtn;
+    Button playball_resetBtn;
     EditText playball_inputNum;
     TextView playball_resStrike;
     TextView playball_resBall;
@@ -36,6 +37,7 @@ public class PlayballActivity extends AppCompatActivity implements TextView.OnEd
     public int firstNum;
     public int secondNum;
     public int thirdNum;
+    public String getNull;
 
     InputMethodManager keyManager;
 
@@ -80,11 +82,11 @@ public class PlayballActivity extends AppCompatActivity implements TextView.OnEd
 
             @Override
             public void onClick(View view) {
-                // 확인 버튼 클릭시 키보드를 내리기 위한 매니저
+                // 확인 버튼 클릭시 키보드(입력창)를 내리기 위한 매니저
                 keyManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
                 keyManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 
-                String getNull = playball_inputNum.getText().toString();
+                getNull = playball_inputNum.getText().toString();
                 if(getNull == null || getNull.equals(""))
                 {
                     Toast.makeText(PlayballActivity.this, "값을 입력하세요.", Toast.LENGTH_SHORT).show();
@@ -147,46 +149,64 @@ public class PlayballActivity extends AppCompatActivity implements TextView.OnEd
             public void onClick(View view) {
 
                 // 헬퍼
-                playball_tableL_hintTableL.setStretchAllColumns(true); // 축소 기능으로 자식 뷰의 개수 또는 크기가 넓어져서 부모의 크기를 넘어서도 각각 자식들의 열너비를 줄여 부모의 전체 크기를 채우도록 하는 속성
-                playball_tableL_hintTableL.setShrinkAllColumns(true); // 확장 기능으로 자식 뷰의 개수 또는 크기가 작아서 부모의 크기에 못미치더라도 각각의 자식들의 열너비를 늘여 부모의 전체 크기를 채우도록 하는 속성
-                TableRow tableRow = new TableRow(PlayballActivity.this);
-                tableRow.setLayoutParams(new TableRow.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT));
-
-                // 여기에 c라이브러리 호출후 배열의 방갯수만큼만 포문 돌려서 화면에 뿌려주기 ㄱㄱ
-                playball_tableL_hintTableL.removeAllViewsInLayout();
-                hintRes = hint(firstNum,secondNum,thirdNum,Integer.parseInt(playball_resStrike.getText().toString()),Integer.parseInt(playball_resBall.getText().toString()));
-
-                int cnt = 0;
-
-                for(int i = 0 ; i < 999; i++)
+                if(getNull == null || getNull.equals(""))
                 {
-                    if(hintRes[i] != -1)
-                    {
-                        TextView textView = new TextView(PlayballActivity.this);
-                        //textView.setText(String.valueOf(random.nextInt(899)+100)); // 테스트 값
-                        textView.setText(String.valueOf(hintRes[i]));
-                        textView.setGravity(Gravity.CENTER);
-                        textView.setPadding(15, 10, 15, 10);
-                        textView.setBackground(getDrawable(R.drawable.hint_textview_background));
-                        tableRow.addView(textView);
-                        cnt++;
-                    }
-                    if(cnt % 10 == 0)
-                    {
-                        playball_tableL_hintTableL.addView(tableRow);
-                        tableRow = new TableRow(PlayballActivity.this);
-                        tableRow.setLayoutParams(new TableRow.LayoutParams(
-                                ViewGroup.LayoutParams.WRAP_CONTENT,
-                                ViewGroup.LayoutParams.WRAP_CONTENT));
-                    }
+                    Toast.makeText(PlayballActivity.this, "입력 후 힌트 확인이 가능합니다.", Toast.LENGTH_SHORT).show();
                 }
-                playball_tableL_hintTableL.addView(tableRow);
-                tableRow = new TableRow(PlayballActivity.this);
-                tableRow.setLayoutParams(new TableRow.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT));
+                else
+                {
+                    playball_tableL_hintTableL.setStretchAllColumns(true); // 축소 기능으로 자식 뷰의 개수 또는 크기가 넓어져서 부모의 크기를 넘어서도 각각 자식들의 열너비를 줄여 부모의 전체 크기를 채우도록 하는 속성
+                    playball_tableL_hintTableL.setShrinkAllColumns(true); // 확장 기능으로 자식 뷰의 개수 또는 크기가 작아서 부모의 크기에 못미치더라도 각각의 자식들의 열너비를 늘여 부모의 전체 크기를 채우도록 하는 속성
+                    TableRow tableRow = new TableRow(PlayballActivity.this);
+                    tableRow.setLayoutParams(new TableRow.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                    // 여기에 c라이브러리 호출후 배열의 방갯수만큼만 포문 돌려서 화면에 뿌려주기 ㄱㄱ
+                    playball_tableL_hintTableL.removeAllViewsInLayout();
+                    hintRes = hint(firstNum, secondNum, thirdNum, Integer.parseInt(playball_resStrike.getText().toString()), Integer.parseInt(playball_resBall.getText().toString()));
+
+                    int cnt = 0;
+
+                    for (int i = 0; i < 999; i++) {
+                        if (hintRes[i] != -1) {
+                            TextView textView = new TextView(PlayballActivity.this);
+                            //textView.setText(String.valueOf(random.nextInt(899)+100)); // 테스트 값
+                            textView.setText(String.valueOf(hintRes[i]));
+                            textView.setGravity(Gravity.CENTER);
+                            textView.setPadding(15, 10, 15, 10);
+                            textView.setBackground(getDrawable(R.drawable.hint_textview_background));
+                            tableRow.addView(textView);
+                            cnt++;
+                        }
+                        if (cnt % 10 == 0) {
+                            playball_tableL_hintTableL.addView(tableRow);
+                            tableRow = new TableRow(PlayballActivity.this);
+                            tableRow.setLayoutParams(new TableRow.LayoutParams(
+                                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                                    ViewGroup.LayoutParams.WRAP_CONTENT));
+                        }
+                    }
+                    playball_tableL_hintTableL.addView(tableRow);
+                    tableRow = new TableRow(PlayballActivity.this);
+                    tableRow.setLayoutParams(new TableRow.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+                }
+            }
+        });
+
+        // 리셋 버튼 리스너
+        playball_resetBtn = findViewById(R.id.playball_btn_resetBtn);
+        playball_resetBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                playball_resStrike.setText("");
+                playball_resBall.setText("");
+                playball_tableL_hintTableL.removeAllViewsInLayout();
+                playball_tableL_historyTableL.removeAllViewsInLayout();
+                dataInit();
 
             }
         });
@@ -197,7 +217,6 @@ public class PlayballActivity extends AppCompatActivity implements TextView.OnEd
 
             @Override
             public void onClick(View view) {
-
                 playball_resultIntent = new Intent();
                 playball_resultIntent.putExtra("playball_back","playball에서 왔어요");
                 setResult(RESULT_OK,playball_resultIntent);
